c53d597a399dda68ccbc695456e11f62cbc38204
Arslan Ahmad <arslanahmadiub@gmail.com>
2023-04-29 23:11:05 +0500

    path added

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const featurePath = process.argv[2];
 const mainPath = process.argv[3];
 
 // Get the contents of the files in the feature and main branches
-const featureFileContents = fs.readFileSync(featurePath, 'utf-8');
-const mainFileContents = execSync(`git show main:${mainPath}`).toString('utf-8');
+// const featureFileContents = fs.readFileSync(featurePath, 'utf-8');
+// const mainFileContents = execSync(`git show main:${mainPath}`).toString('utf-8');
 
 console.log("mainPath",mainPath)
 console.log("featurePath",featurePath)
 
 
 // Compare the contents of the files
-if (featureFileContents === mainFileContents) {
-  console.log('The files in the feature and main branches have the same contents.');
-} else {
-  console.log('The files in the feature and main branches have different contents.');
-}
+// if (featureFileContents === mainFileContents) {
+//   console.log('The files in the feature and main branches have the same contents.');
+// } else {
+//   console.log('The files in the feature and main branches have different contents.');
+// }
 // git push https://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@github.com/arslanahmadiub/TestRepo.git

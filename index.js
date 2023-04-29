17b90123bbe7d8bb81dfded25ff9b3997290795b
Arslan Ahmad <arslanahmadiub@gmail.com>
2023-04-29 22:41:32 +0500

    add new code

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let a = 5;
-let b =10
-let c = a+b
-console.log("Sum", clearTimeout)
+
+console.log("I am here")
+
+// const fs = require('fs');
+// const { execSync } = require('child_process');
+
+// // Get the paths to the feature and main branch files from command-line arguments
+// const featurePath = process.argv[2];
+// const mainPath = process.argv[3];
+
+// // Get the contents of the files in the feature and main branches
+// const featureFileContents = fs.readFileSync(featurePath, 'utf-8');
+// const mainFileContents = execSync(`git show main:${mainPath}`).toString('utf-8');
+
+// // Compare the contents of the files
+// if (featureFileContents === mainFileContents) {
+//   console.log('The files in the feature and main branches have the same contents.');
+// } else {
+//   console.log('The files in the feature and main branches have different contents.');
+// }
+// git push https://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@github.com/arslanahmadiub/TestRepo.git

c3eb7050e5b7e04514b569ff9d51afca4fe9af8f
Arslan Ahmad <arslanahmadiub@gmail.com>
2023-04-29 23:09:07 +0500

    update index

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 
 console.log("I am here")
 
-// const fs = require('fs');
-// const { execSync } = require('child_process');
+const fs = require('fs');
+const { execSync } = require('child_process');
 
-// // Get the paths to the feature and main branch files from command-line arguments
-// const featurePath = process.argv[2];
-// const mainPath = process.argv[3];
+// Get the paths to the feature and main branch files from command-line arguments
+const featurePath = process.argv[2];
+const mainPath = process.argv[3];
 
-// // Get the contents of the files in the feature and main branches
-// const featureFileContents = fs.readFileSync(featurePath, 'utf-8');
-// const mainFileContents = execSync(`git show main:${mainPath}`).toString('utf-8');
+// Get the contents of the files in the feature and main branches
+const featureFileContents = fs.readFileSync(featurePath, 'utf-8');
+const mainFileContents = execSync(`git show main:${mainPath}`).toString('utf-8');
 
-// // Compare the contents of the files
-// if (featureFileContents === mainFileContents) {
-//   console.log('The files in the feature and main branches have the same contents.');
-// } else {
-//   console.log('The files in the feature and main branches have different contents.');
-// }
+console.log("mainPath",mainPath)
+console.log("featurePath",featurePath)
+
+
+// Compare the contents of the files
+if (featureFileContents === mainFileContents) {
+  console.log('The files in the feature and main branches have the same contents.');
+} else {
+  console.log('The files in the feature and main branches have different contents.');
+}
 // git push https://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@github.com/arslanahmadiub/TestRepo.git

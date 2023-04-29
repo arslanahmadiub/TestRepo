3235a51fcffd9df1d2d08a05c0fd081173b69c0e
Arslan Ahmad <arslanahmadiub@gmail.com>
2023-04-29 23:43:08 +0500

    old content

--- index.js
+++ index.js
@@ -7,27 +7,18 @@ const masterBranch = 'master';
 const featureBranch = 'compare-branch';
 
 // Get the content of the file in master branch
-// git.show([`${masterBranch}:${filePath}`], (err, masterContent) => {
-//   if (err) throw err;
-//   console.log("masterContent", masterContent)
+git.show([`${masterBranch}:${filePath}`], (err, masterContent) => {
+  if (err) throw err;
 
-//   // Get the content of the file in feature branch
-//   git.show([`${featureBranch}:${filePath}`], (err, featureContent) => {
-//     if (err) throw err;
-// console.log("Feature Contenet", featureContent)
-//     // Compare the content of the two files
-//     // if (masterContent === featureContent) {
-//     //   console.log('The file content is the same in both branches.');
-//     // } else {
-//     //   console.log('The file content is different in the two branches.');
-//     // }
-//   });
-// });
+  // Get the content of the file in feature branch
+  git.show([`${featureBranch}:${filePath}`], (err, featureContent) => {
+    if (err) throw err;
 
-console.log('PrintBranchName', git.show([`${featureBranch}`]))
-
-// git.show([`${featureBranch}:${filePath}`], (err, masterContent) => {
-//     if (err) throw err;
-//     console.log("masterContent", masterContent)
-  
-//   });
+    // Compare the content of the two files
+    if (masterContent === featureContent) {
+      console.log('The file content is the same in both branches.');
+    } else {
+      console.log('The file content is different in the two branches.');
+    }
+  });
+});

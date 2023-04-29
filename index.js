28948096b1644b333d9b3bc95686f4e97dcc03b0
Arslan Ahmad <arslanahmadiub@gmail.com>
2023-04-29 23:29:54 +0500

    new show

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ const simpleGit = require('simple-git');
 const git = simpleGit();
 
 // Define the file path and branch names
-const filePath = 'test.js';
+const filePath = '/test.js';
 const masterBranch = 'master';
 const featureBranch = 'compare-branch';
 
 // Get the content of the file in master branch
-git.show([`${masterBranch}:${filePath}`], (err, masterContent) => {
-  if (err) throw err;
-  console.log("masterContent", masterContent)
+// git.show([`${masterBranch}:${filePath}`], (err, masterContent) => {
+//   if (err) throw err;
+//   console.log("masterContent", masterContent)
 
-  // Get the content of the file in feature branch
-  git.show([`${featureBranch}:${filePath}`], (err, featureContent) => {
+//   // Get the content of the file in feature branch
+//   git.show([`${featureBranch}:${filePath}`], (err, featureContent) => {
+//     if (err) throw err;
+// console.log("Feature Contenet", featureContent)
+//     // Compare the content of the two files
+//     // if (masterContent === featureContent) {
+//     //   console.log('The file content is the same in both branches.');
+//     // } else {
+//     //   console.log('The file content is different in the two branches.');
+//     // }
+//   });
+// });
+
+git.show([`${featureBranch}:${filePath}`], (err, masterContent) => {
     if (err) throw err;
-console.log("Feature Contenet", featureContent)
-    // Compare the content of the two files
-    // if (masterContent === featureContent) {
-    //   console.log('The file content is the same in both branches.');
-    // } else {
-    //   console.log('The file content is different in the two branches.');
-    // }
+    console.log("masterContent", masterContent)
+  
   });
-});

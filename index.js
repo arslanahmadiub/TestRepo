79863fea52089f06ad60bd5f63a4b1e2e53bbf37
Arslan Ahmad <arslanahmadiub@gmail.com>
2023-05-03 11:50:45 +0500

    change pipeline

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const filePath = 'test.js';
 const masterBranch = 'master';
 const featureBranch = 'compare-branch';
 
+const regex = /let\s+a\s*=\s*(\d+);?\s*/;
+
+
 // Get the content of the file in master branch
 git.show([`${masterBranch}:${filePath}`], (err, masterContent) => {
   if (err) throw err;
-    console.log("MasterContent", masterContent)
+  const masterValue = masterContent.match(regex)[1];
+  console.log("MasterValue", masterValue);
   // Get the content of the file in feature branch
   git.show([`${featureBranch}:${filePath}`], (err, featureContent) => {
     if (err) throw err;
-    console.log("featureContent", featureContent)
-
+    const featureValue = featureContent.match(regex)[1];
+    console.log("FeatureValue", featureValue);
     // Compare the content of the two files
     if (masterContent === featureContent) {
       console.log('The file content is the same in both branches.');
